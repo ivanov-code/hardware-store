d3db859bfda758ee9142147939ba39c28995b1d7
danil_ivanov <danil_ivanov@epam.com>
2021-10-15 16:09:31 +0500

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.scss';
+import cssClasses from './app.module.scss';
+import {FC} from "react";
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
-
-export default App;
+export const App: FC = () => {
+    return (
+        <div className={cssClasses.layout}>
+            <header className={cssClasses.layout__header}>
+                header
+            </header>
+            <main className={cssClasses.layout__content}>
+                main
+            </main>
+            <footer className={cssClasses.layout__footer}>
+                footer
+            </footer>
+        </div>
+    );
+};
